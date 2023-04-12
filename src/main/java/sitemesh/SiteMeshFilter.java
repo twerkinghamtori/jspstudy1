2b3454b87098d0bc380e97a524e256a3361444c5
강수빈 <zxc2289@naver.com>
2023-04-12 16:34:25 +0900

    jspstudy1 commit

--- src/main/java/sitemesh/SiteMeshFilter.java
+++ src/main/java/sitemesh/SiteMeshFilter.java
@@ -3,17 +3,17 @@ package sitemesh;
  * sitemesh 기능 : 화면에 layout을 코딩한 jsp를 페이지에 적용해줌.
  * 
  * addDecoratorPath(URL, layout)
- *   url : (jsptstudy1)/model/member/ 로 시작하는 모든 요청 url
+ *   url : /member/ 로 시작하는 모든 요청 url
  *   layout : 응답시 페이지 layout용으로 사용되는 jsp 페이지 설정.
  * 
- * addExcludedPath("model1/member/id*")
- *   url : model1/member/id 로 시작하는 모든 요청시 layout 제외*/
+ * addExcludedPath("/member/id*")
+ *   url : /member/id 로 시작하는 모든 요청시 layout 제외*/
 import javax.servlet.annotation.WebFilter;
 import org.sitemesh.builder.SiteMeshFilterBuilder;
 import org.sitemesh.config.ConfigurableSiteMeshFilter;
 
 @WebFilter("/*") //모든 요청(/*)이 들어오면 이 필터를 실행한다.
-public class SiteMeshFilter extends ConfigurableSiteMeshFilter {
+public class SiteMeshFilter extends ConfigurableSiteMeshFilter {    
     @Override
     protected void applyCustomConfiguration(SiteMeshFilterBuilder builder) {
     	builder.addDecoratorPath("/model1/*", "/layout/layout.jsp")
@@ -23,3 +23,5 @@ public class SiteMeshFilter extends ConfigurableSiteMeshFilter {
     	.addExcludedPath("/model1/member/picture*");
     }
 } 
+
+
--- src/main/webapp/model1/member/delete.jsp
+++ src/main/webapp/model1/member/delete.jsp
@@ -2,21 +2,6 @@
 <%@page import="model.MemberDao"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%-- 1. 파라미터 정보 저장
-     2. 로그인정보 검증
-          -로그아웃 상태 : 로그인하세요. 메세지 출력 후 loginForm.jsp로 이동
-          -다른사람 탈퇴 시도, 관리자제외 : 본인만 탈퇴 가능합니다. 메세치 출력 후 main.jsp로 이동
-     3. 관리자 탈퇴는 불가.
-          - 관리자 정보 탈퇴 시 관리자는 탈퇴 불가. 메세지 출력 후 list.jsp로 이동
-     4. 비밀번호 검증
-          - 로그인 정보로 비밀번호 검증.
-               -비밀번호가 틀림 : 비밀번호가 틀렸습니다 출력 후 deleteForm.jsp로 이동
-               -비밀번호 맞음 : db에서 delete 실행. (MemberDao delete(id))
-                        -일반사용자 : 로그아웃.(세션정보 종료) 탈퇴가 완료되었습니다.출력 후 loginForm.jsp로 이동
-                        -관리자 : 탈퇴 완료 메세지 출력 후 list.jsp로 이동
-     5. 탈퇴 실패 - 일반사용자 : 탈퇴 실패 메세지 출력 후 info.jsp로 이동
-                - 관리자 : 탈퇴 실패 메세지 출력 후 list.jsp로 이동
---%>
 <%
    request.setCharacterEncoding("UTF-8");
    String id = request.getParameter("id");
